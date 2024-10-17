c279fcf0ac79f8ea2a7085aae76bd3516ce37853
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-10-18 02:12:27 +0300

    feat 4 models, .gitignore and settings.py edit

--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    platform = models.CharField(max_length=100, null=True, blank=True)
+    release_year = models.DateField(null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        related_name="games",
+    )
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Gamer(AbstractUser):
+    def __str__(self) -> str:
+        return self.username
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    gamer = models.ForeignKey(
+        Gamer,
+        on_delete=models.CASCADE,
+        related_name="collections",
+    )
+    game = models.ManyToManyField(Game, related_name="collections")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
