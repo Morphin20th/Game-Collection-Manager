fbcce158c6f26ee94a2cc3f359571df7574f91d2
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-10-19 02:13:02 +0300

    add django-debug-toobar

--- Game_Collection_Manager/urls.py
+++ Game_Collection_Manager/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("collection.urls", namespace="collection")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
